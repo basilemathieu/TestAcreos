bdca9279645b342b5d67c89ab2c46ba00e147496
Basile Mathieu <basilemathieu@gmail.com>
2018-07-25 14:51:23 +0200

    Cleaning Hook features

--- TEST_UNITY/Assets/Scripts/Crane/hookDriver.cs
+++ TEST_UNITY/Assets/Scripts/Crane/hookDriver.cs
@@ -2,39 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(FixedJoint))]
-public class hookDriver : MonoBehaviour {
-    private FixedJoint m_fixedjoint;
+[RequireComponent(typeof(Rigidbody))]
+public class HookDriver : MonoBehaviour
+{
+    private Rigidbody m_rigidBody;
+    private FixedJoint m_fixedJoint;
     private bool hookFree = true;
+    [SerializeField]
+    private float m_hookDuration = 2.0f;
+    private float m_hookingTime = 0.0f;
+    private Load m_currentLoad;
+    private Coroutine currentTimer;
 
     private void Start()
     {
-        m_fixedjoint = GetComponent<FixedJoint>();
+        //set up rigid body for fixed joint
+        m_rigidBody = GetComponent<Rigidbody>();
+        m_rigidBody.useGravity = false;
+        m_rigidBody.isKinematic = true;
     }
 
     // Update is called once per frame
-    void Update () {
-        if (Input.GetKeyDown("Fire1"))
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
         {
             if (hookFree == false)
             {
                 ReleaseHook();
             }
         }
-		
-	}
 
-    public void CloseHook(Load hookedLoad)
+    }
+    public void BeginHook(Load hookedLoad)
     {
         if (hookFree)
         {
-            m_fixedjoint.connectedBody = hookedLoad.GetComponent<Rigidbody>();
-            hookFree = false;
+            m_currentLoad = hookedLoad;
+            currentTimer = StartCoroutine(TimeHooking(hookedLoad));
         }
+    }
+    public void StopHook(Load hookedLoad)
+    {
+        if (hookFree && m_currentLoad == hookedLoad)
+        {
+            StopCoroutine(currentTimer);
+            m_hookingTime = 0.0f;
+            m_currentLoad = null;
+        }
+    }
+
+    private IEnumerator TimeHooking(Load hookedLoad) 
+    {
+        while (m_hookingTime < m_hookDuration)//waiting the needed time
+        {
+            m_hookingTime += Time.deltaTime;
+            yield return null;
+        }
+        CloseHook(hookedLoad);
+        m_hookingTime = 0.0f;
+        m_currentLoad = null;
+        yield break;
+    }
+
+    public void CloseHook(Load hookedLoad)
+    {
+
+        hookedLoad.transform.position = transform.position; //center load on hook 
+        m_fixedJoint = gameObject.AddComponent<FixedJoint>(); //creating the joint
+        m_fixedJoint.connectedBody = hookedLoad.GetComponent<Rigidbody>();//attaching the load
+        hookFree = false;
+
     }
     private void ReleaseHook()
     {
-        m_fixedjoint.connectedBody = null; //not working
+        Destroy(m_fixedJoint); //freeing the load
+
         hookFree = true;
     }
 }
--- TEST_UNITY/Assets/Scripts/loads/Load.cs
+++ TEST_UNITY/Assets/Scripts/loads/Load.cs
@@ -7,74 +7,38 @@ public class Load : MonoBehaviour
     private string m_loadName;
     [SerializeField]
     private float m_loadMass;
-    private SphereCollider m_sphereCollider;
-    private float m_hookingDuration = 2.0f;
+    [SerializeField]
     private float m_hookMaxRange = 0.5f;
-
-    private float m_hookingTime = 0.0f;
-    private Coroutine m_hookTimer;
-    private hookDriver m_hook;
+    private HookDriver m_hook;
     private bool m_hookInRange = false;
+    private bool m_hookInRangePrevValue;
 
 
     private void Start()
     {
-        m_hook = FindObjectOfType<hookDriver>();
+        m_hook = FindObjectOfType<HookDriver>();
     }
 
     private void Update()
     {
-        m_hookInRange = Vector3.Distance(this.transform.position, m_hook.transform.position) <= m_hookMaxRange;
-
-        if (m_hookInRange && m_hookingTime < m_hookingDuration)
+        m_hookInRange = Vector3.Distance(this.transform.position, m_hook.transform.position) <= m_hookMaxRange; //checking the range
+        if (m_hookInRange != m_hookInRangePrevValue) //only if the state changed
         {
-            m_hookingTime += Time.deltaTime;
-        }
-        else if (m_hookInRange)
-        {
-            m_hook.CloseHook(this);
-        }
-        else
-        {
-            m_hookingTime = 0.0f;
-        }
-
-
-
-
-    }
-
-
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Hook"))
-        {
-            m_hookTimer = StartCoroutine(CountHookTime());
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Hook"))
-        {
-            if (m_hookingTime < m_hookingDuration)
+            if (m_hookInRange)
             {
-                StopCoroutine(m_hookTimer);
-                m_hookingTime = 0.0f;
+                m_hook.BeginHook(this); //try to hook load
+            }
+            else
+            {
+                m_hook.StopHook(this); //quit trying
             }
         }
-    }
-
-    private IEnumerator CountHookTime()
-    {
-        while (m_hookingTime < m_hookingDuration)
-        {
-            m_hookingTime += Time.deltaTime;
-            yield return null;
-        }
-
-        //hook
+        m_hookInRangePrevValue = m_hookInRange; //recording state
     }
 
 }
+
+
+
+
+
